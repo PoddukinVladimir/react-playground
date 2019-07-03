32a6e7a4ef3c2200978575f5fe129f6b437fdc18
Пользователь <poddukinvladimir@gmail.com>
2019-07-03 22:34:13 +0300

    add build folder

---
+++ build/precache-manifest.1f3114a61bf37403cb56102358abf671.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e70c42a1f1c5061e51aa808dd368111",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a1562d3ce0b08037b899",
+    "url": "/static/css/main.8d1fe082.chunk.css"
+  },
+  {
+    "revision": "e57bdfd407085a7f2a85",
+    "url": "/static/js/2.a140dcbf.chunk.js"
+  },
+  {
+    "revision": "a1562d3ce0b08037b899",
+    "url": "/static/js/main.95b8042e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
